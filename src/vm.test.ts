1aa2cd6fecc6750c53355b1327450026db45edee
airtoxin <airtoxin@icloud.com>
2024-11-10 05:10:36 +0900

    Fix compile

--- src/vm.test.ts
+++ src/vm.test.ts
@@ -3,8 +3,7 @@ import { Vm } from "./vm";
 import { run } from "./module";
 
 describe("vm", () => {
-  test("test", () => {
-    const vm = new Vm();
+  test("simple wiring", () => {
     const program = `\
       VAR in0 BITIN
       VAR in1 BITIN
@@ -16,7 +15,9 @@ describe("vm", () => {
       FROM in1 o0 TO nand i1
       FROM nand o0 TO out i0
     `;
-    const global = vm.compile(program);
+    const vm = new Vm(program);
+    const globalModule = vm.compile();
+    const global = globalModule.createVariable("_");
 
     expect(
       run(
@@ -41,6 +42,52 @@ describe("vm", () => {
         ]),
       ),
     ).toMatchInlineSnapshot(`
+      Map {
+        "out" => false,
+      }
+    `);
+  });
+
+  test("custom module", () => {
+    const program = `\
+      VAR in0 BITIN
+      VAR out BITOUT
+      
+      MOD START NOT
+        VAR in BITIN
+        VAR out BITOUT
+        
+        MOD START NOT
+          VAR in BITIN
+          VAR out BITOUT
+          VAR nand NAND
+          
+          FROM in o0 TO nand i0
+          FROM in o0 TO nand i1
+          FROM nand o0 TO out i0
+        MOD END
+        VAR not NOT
+        
+        FROM in o0 TO not in
+        FROM not out TO out i0
+      MOD END
+      
+      VAR not NOT
+      
+      FROM in0 o0 TO not in
+      FROM not out TO out i0
+    `;
+    const vm = new Vm(program);
+    const globalModule = vm.compile();
+    const global = globalModule.createVariable("_");
+
+    expect(run(global, new Map([["in0", false]]))).toMatchInlineSnapshot(`
+      Map {
+        "out" => true,
+      }
+    `);
+
+    expect(run(global, new Map([["in0", true]]))).toMatchInlineSnapshot(`
       Map {
         "out" => false,
       }
--- src/vm.ts
+++ src/vm.ts
@@ -9,23 +9,39 @@ import {
   Variable,
 } from "./module";
 import { Reactive } from "@reactively/core";
+import { Token } from "./token";
 
 export class Vm {
-  public compile(program: string): Variable {
-    const tokens = lex(program);
+  private tokens: Token[];
+  constructor(private program: string) {
+    this.tokens = lex(this.program);
+  }
 
-    const inPorts = new Map<string, Reactive<boolean>>();
-    const outPorts = new Map<string, Reactive<boolean>>();
-    const modules = new Map<string, Module>([
+  public compile(): Module {
+    const globalModules = new Map<string, Module>([
       ["NAND", nandModule],
       ["BITIN", bitinModule],
       ["BITOUT", bitoutModule],
     ]);
+    return this._compile("GLOBAL", globalModules);
+  }
+
+  private _compile(
+    moduleName: string,
+    parentModules: Map<string, Module>,
+  ): Module {
+    const inPorts = new Map<string, Reactive<boolean>>();
+    const outPorts = new Map<string, Reactive<boolean>>();
+    const modules = new Map<string, Module>();
     const variables = new Map<string, Variable>();
 
-    for (const token of tokens) {
+    while (this.tokens.length > 0) {
+      const token = this.tokens.shift();
+      if (token == null) break;
+
       if (token.type === "variable") {
-        const mod = modules.get(token.moduleName);
+        const mod =
+          modules.get(token.moduleName) ?? parentModules.get(token.moduleName);
         if (mod == null)
           throw new Error(`Unknown module name: ${token.moduleName}`);
         const variable = mod.createVariable(token.name);
@@ -49,13 +65,39 @@ export class Vm {
         if (srcPort == null || destPort == null)
           throw new Error(`Unknown wiring port. ${JSON.stringify(token)}`);
         destPort.set(() => srcPort.get());
+      } else if (token.type === "moduleStart") {
+        modules.set(
+          token.name,
+          this._compile(
+            token.name,
+            new Map([...parentModules.entries(), ...modules.entries()]),
+          ),
+        );
+      } else if (token.type === "moduleEnd") {
+        return {
+          name: moduleName,
+          modules: [],
+          createVariable: (name) => {
+            return {
+              name,
+              inPorts,
+              outPorts,
+            };
+          },
+        };
       }
     }
 
     return {
-      name: "GLOBAL",
-      inPorts,
-      outPorts,
+      name: moduleName,
+      modules: [],
+      createVariable: (name) => {
+        return {
+          name,
+          inPorts,
+          outPorts,
+        };
+      },
     };
   }
 }
