3b69390b4f5ffdf2e8cb86c3282b0661394dd567
airtoxin <airtoxin@icloud.com>
2024-11-10 05:42:15 +0900

    Add test

--- src/vm.test.ts
+++ src/vm.test.ts
@@ -48,46 +48,133 @@ describe("vm", () => {
     `);
   });
 
-  test("custom module", () => {
+  test("NOT GATE", () => {
     const program = `\
-      VAR in0 BITIN
+      VAR in BITIN
       VAR out BITOUT
       
       MOD START NOT
         VAR in BITIN
         VAR out BITOUT
+        VAR nand NAND
         
-        MOD START NOT
-          VAR in BITIN
-          VAR out BITOUT
-          VAR nand NAND
-          
-          FROM in o0 TO nand i0
-          FROM in o0 TO nand i1
-          FROM nand o0 TO out i0
-        MOD END
-        VAR not NOT
-        
-        FROM in o0 TO not in
-        FROM not out TO out i0
+        FROM in o0 TO nand i0
+        FROM in o0 TO nand i1
+        FROM nand o0 TO out i0
       MOD END
       
       VAR not NOT
       
-      FROM in0 o0 TO not in
+      FROM in o0 TO not in
       FROM not out TO out i0
     `;
     const vm = new Vm(program);
     const globalModule = vm.compile();
     const global = globalModule.createVariable("_");
 
-    expect(run(global, new Map([["in0", false]]))).toMatchInlineSnapshot(`
+    expect(run(global, new Map([["in", true]]))).toMatchInlineSnapshot(`
+      Map {
+        "out" => false,
+      }
+    `);
+
+    expect(run(global, new Map([["in", false]]))).toMatchInlineSnapshot(`
+      Map {
+        "out" => true,
+      }
+    `);
+  });
+
+  test("AND GATE", () => {
+    const program = `\
+      VAR in0 BITIN
+      VAR in1 BITIN
+      VAR out BITOUT
+      
+      MOD START NOT
+        VAR in BITIN
+        VAR out BITOUT
+        VAR nand NAND
+        
+        FROM in o0 TO nand i0
+        FROM in o0 TO nand i1
+        FROM nand o0 TO out i0
+      MOD END
+      
+      MOD START AND
+        VAR in0 BITIN
+        VAR in1 BITIN
+        VAR out BITOUT
+        VAR nand NAND
+        VAR not NOT
+        
+        FROM in0 o0 TO nand i0
+        FROM in1 o0 TO nand i1
+        FROM nand o0 TO not in
+        FROM not out TO out i0
+      MOD END
+      
+      VAR and AND
+      
+      FROM in0 o0 TO and in0
+      FROM in1 o0 TO and in1
+      FROM and out TO out i0
+    `;
+    const vm = new Vm(program);
+    const globalModule = vm.compile();
+    const global = globalModule.createVariable("_");
+
+    expect(
+      run(
+        global,
+        new Map([
+          ["in0", true],
+          ["in1", true],
+        ]),
+      ),
+    ).toMatchInlineSnapshot(`
       Map {
         "out" => true,
       }
     `);
 
-    expect(run(global, new Map([["in0", true]]))).toMatchInlineSnapshot(`
+    expect(
+      run(
+        global,
+        new Map([
+          ["in0", false],
+          ["in1", true],
+        ]),
+      ),
+    ).toMatchInlineSnapshot(`
+      Map {
+        "out" => false,
+      }
+    `);
+
+    expect(
+      run(
+        global,
+        new Map([
+          ["in0", true],
+          ["in1", false],
+        ]),
+      ),
+    ).toMatchInlineSnapshot(`
+      Map {
+        "out" => false,
+      }
+    `);
+
+    expect(
+      run(
+        global,
+        new Map([
+          ["in0", false],
+          ["in1", false],
+        ]),
+      ),
+    ).toMatchInlineSnapshot(`
       Map {
         "out" => false,
       }
