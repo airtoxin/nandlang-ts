2bf2d69c1449f444cc1acb1660edf6c4631f348b
airtoxin <airtoxin@icloud.com>
2024-11-09 15:53:24 +0900

    Fix index access

--- src/lexer.ts
+++ src/lexer.ts
@@ -10,14 +10,27 @@ export const lex = (program: string): Token[] => {
   const tokens: Token[] = [];
   for (const line of program.split("\n")) {
     const words = line.trim().split(/\s+/);
-    if (words[0] === "VAR" && words.length === 3) {
+    if (
+      words[0] === "VAR" &&
+      typeof words[1] === "string" &&
+      typeof words[2] === "string" &&
+      words.length === 3
+    ) {
       const token: VariableToken = {
         type: "variable",
         name: words[1],
         module_name: words[2],
       };
       tokens.push(token);
-    } else if (words[0] === "FROM" && words[3] === "TO" && words.length === 6) {
+    } else if (
+      words[0] === "FROM" &&
+      typeof words[1] === "string" &&
+      typeof words[2] === "string" &&
+      words[3] === "TO" &&
+      typeof words[4] === "string" &&
+      typeof words[5] === "string" &&
+      words.length === 6
+    ) {
       const token: WireToken = {
         type: "wire",
         src_variable_name: words[1],
@@ -29,6 +42,7 @@ export const lex = (program: string): Token[] => {
     } else if (
       words[0] === "MOD" &&
       words[1] === "START" &&
+      typeof words[2] === "string" &&
       words.length === 3
     ) {
       const token: ModuleStartToken = {
