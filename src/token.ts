6834437a5c668e99e282726d0272f2aa0506cd1f
airtoxin <airtoxin@icloud.com>
2024-11-09 15:14:13 +0900

    Add token

---
+++ src/token.ts
@@ -0,0 +1,28 @@
+export type Token =
+  | VariableToken
+  | WireToken
+  | ModuleStartToken
+  | ModuleEndToken
+
+export type VariableToken = {
+  type: "variable";
+  name: string;
+  module_name: string;
+};
+
+export type WireToken = {
+  type: "wire";
+  src_variable_name: string;
+  src_variable_port: string;
+  dest_variable_name: string;
+  dest_variable_port: string;
+};
+
+export type ModuleStartToken = {
+  type: "moduleStart";
+  name: string;
+};
+
+export type ModuleEndToken = {
+  type: "moduleEnd";
+};
