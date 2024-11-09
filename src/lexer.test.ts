bcdd6d4255b8ec060af421d730057061a1f58811
airtoxin <airtoxin@icloud.com>
2024-11-09 15:36:52 +0900

    Add lexer

---
+++ src/lexer.test.ts
@@ -0,0 +1,69 @@
+import {describe, expect, test} from "vitest";
+import {lex} from "./lexer";
+
+describe("lex", () => {
+  test("wiring", () => {
+    const program = `\
+      FROM a out TO x in
+    `;
+    expect(lex(program)).toMatchInlineSnapshot(`
+      [
+        {
+          "dest_variable_name": "x",
+          "dest_variable_port": "in",
+          "src_variable_name": "a",
+          "src_variable_port": "out",
+          "type": "wire",
+        },
+      ]
+    `);
+  });
+
+  test("var", () => {
+    const program = `\
+      VAR x NAND
+    `;
+    expect(lex(program)).toMatchInlineSnapshot(`
+      [
+        {
+          "module_name": "NAND",
+          "name": "x",
+          "type": "variable",
+        },
+      ]
+    `);
+  });
+
+  test("module def", () => {
+    const program = `\
+      MOD START TRASH
+        VAR in BITIN
+      MOD END
+    `;
+    expect(lex(program)).toMatchInlineSnapshot(`
+      [
+        {
+          "name": "TRASH",
+          "type": "moduleStart",
+        },
+        {
+          "module_name": "BITIN",
+          "name": "in",
+          "type": "variable",
+        },
+        {
+          "type": "moduleEnd",
+        },
+      ]
+    `);
+  });
+
+  test("comment", () => {
+    const program = `\
+      # MOD START TRASH
+      #  VAR in BITIN
+      # MOD END
+    `;
+    expect(lex(program)).toMatchInlineSnapshot(`[]`);
+  });
+});
---
+++ src/lexer.ts
@@ -0,0 +1,40 @@
+import {ModuleEndToken, ModuleStartToken, Token, VariableToken, WireToken} from "./token";
+
+export const lex = (program: string): Token[] => {
+  const tokens: Token[] = [];
+  for (const line of program.split("\n")) {
+    const words = line.trim().split(/\s+/);
+    if (words[0] === "VAR" && words.length === 3) {
+      const token: VariableToken = {
+        type: "variable",
+        name: words[1],
+        module_name: words[2]
+      }
+      tokens.push(token);
+    } else if (words[0] === "FROM" && words[3] === "TO" && words.length === 6) {
+      const token: WireToken = {
+        type: "wire",
+        src_variable_name: words[1],
+        src_variable_port: words[2],
+        dest_variable_name: words[4],
+        dest_variable_port: words[5],
+      }
+      tokens.push(token);
+    } else if (words[0] === "MOD" && words[1] === "START" && words.length === 3) {
+      const token: ModuleStartToken = {
+        type: "moduleStart",
+        name: words[2],
+      }
+      tokens.push(token);
+    } else if (words[0] === "MOD" && words[1] === "END" && words.length === 2) {
+      const token: ModuleEndToken = {
+        type: "moduleEnd",
+      }
+      tokens.push(token);
+    } else if (words[0] === "#") {
+      // ignore comment line
+    }
+  }
+
+  return tokens;
+};
