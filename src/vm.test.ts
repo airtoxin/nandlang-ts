4eb275c7925cafbcfc5a09b4157a47f75201c9e8
airtoxin <airtoxin@icloud.com>
2024-11-10 12:05:28 +0900

    Add and

--- src/vm.test.ts
+++ src/vm.test.ts
@@ -107,4 +107,99 @@ describe("vm", () => {
       }
     `);
   });
+
+  test("AND GATE", () => {
+    const program = `\      
+      MOD START AND
+        MOD START NOT
+          VAR in BITIN
+          VAR out BITOUT
+          VAR nand NAND
+          
+          FROM in _ TO nand i0
+          FROM in _ TO nand i1
+          FROM nand _ TO out _
+        MOD END
+        
+        VAR in0 BITIN
+        VAR in1 BITIN
+        VAR nand NAND
+        VAR not NOT
+        VAR out BITOUT
+        
+        FROM in0 _ TO nand i0
+        FROM in1 _ TO nand i1
+        FROM nand _ TO not _
+        FROM not _ TO out _
+      MOD END
+      
+      VAR in0 BITIN
+      VAR in1 BITIN
+      VAR and AND
+      VAR out BITOUT
+      
+      FROM in0 _ TO and in0
+      FROM in1 _ TO and in1
+      FROM and _ TO out _
+    `;
+    const vm = new Vm(program);
+    const globalModule = vm.compile();
+    const global = globalModule.createVariable("_");
+
+    expect(
+      run(
+        global,
+        new Map([
+          ["in0", false],
+          ["in1", false],
+        ]),
+      ),
+    ).toMatchInlineSnapshot(`
+      Map {
+        "out" => false,
+      }
+    `);
+
+    expect(
+      run(
+        global,
+        new Map([
+          ["in0", false],
+          ["in1", true],
+        ]),
+      ),
+    ).toMatchInlineSnapshot(`
+      Map {
+        "out" => false,
+      }
+    `);
+
+    expect(
+      run(
+        global,
+        new Map([
+          ["in0", true],
+          ["in1", false],
+        ]),
+      ),
+    ).toMatchInlineSnapshot(`
+      Map {
+        "out" => false,
+      }
+    `);
+
+    expect(
+      run(
+        global,
+        new Map([
+          ["in0", true],
+          ["in1", true],
+        ]),
+      ),
+    ).toMatchInlineSnapshot(`
+      Map {
+        "out" => true,
+      }
+    `);
+  });
 });
